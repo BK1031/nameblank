4a7beea7fa87a08a86e8ec4ec9b067c09b128cf5
Bharat Kathi <bharat1031@gmail.com>
2024-04-16 21:39:19 -0700

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Loader2 } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <div className=""></div>
+      <div className="flex">
+        <div className="flex flex-col p-16">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+            nameblank
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
