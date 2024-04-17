0300da9da094cf848704abb57a56e42926dc842b
Bharat Kathi <bharat1031@gmail.com>
2024-04-16 21:48:34 -0700

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { Toaster } from "./components/ui/sonner.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 const router = createBrowserRouter([
   {
@@ -16,5 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <Toaster />
+    <Analytics />
   </React.StrictMode>,
 );
