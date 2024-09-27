ea414795d46c2039b88a672e7b47c6dc3e0476e2
Z z <zz@Zs-MacBook-Air.local>
2024-09-26 22:34:26 -0700

    my changes

---
+++ src/About.tsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center max-w-2xl menlo font-light">
+        <p className="text-lg mb-3">I don't really think there's much to it, I'm from West Hollywood, CA and I like to make clothes that actually feel like something. Just under two years ago I realized there was nothing I owned or could buy that felt like me.</p>
+        <p className="text-lg mb-3">So often when you buy clothes you think of how others would see you in them or how they've looked on others, and each piece is put into a category. There's words like 'style', 'fit', 'gender', 'season', and certainly many more that contribute to clothes being seen in an unhealthy way. I don't need someone telling me how to wear MY clothes, and I'm sure others feel the same. </p>
+        <p className="text-lg mb-3">All my clothes are unisex and can be tailored to any given person's unique expression. </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
